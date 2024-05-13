02fab76652a8e1e01b2ec07a42e9942061153119
FatahNurhy-PPLG1 <113590794+FatahNurhy-PPLG1@users.noreply.github.com>
2024-05-13 11:33:14 +0700

    Add files via upload

---
+++ tugas2.php
@@ -0,0 +1,69 @@
+<?php 
+$jurusan = [
+    [
+      "nama" => "RPL",
+      "siswa" => [
+        ["nama" => "satria", "umur" => "18 tahun"],
+        ["nama" => "fattah", "umur" => "19 tahun"],
+      ],
+    ],
+
+    [
+      "nama" => "TKJ",
+      "siswa" => [
+          ["nama" => "shakira", "umur" => "17 tahun"],
+          ["nama" => "marsina", "umur" => "20 tahun"],
+      ],
+    ],
+
+    [
+      "nama" => "TKR",
+      "siswa" => [
+          ["nama" => "Raden", "umur" => "16 tahun"],
+          ["nama" => "Fariz", "umur" => "20 tahun"],
+      ]
+    ]
+  ];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+</head>
+
+<body>
+  <div class="container my-5">
+    
+      <h1>Data Siswa</h1>
+      <?php foreach ($jurusan as $jurusan){?>
+    <table class="table">
+      <h1> <?php echo $jurusan["nama"]?> </h1>
+      <thead>
+        <tr>
+          <th scope="col">Nama</th>
+          <th scope="col">Umur</th>
+        </tr>
+      </thead>
+      <tbody>
+      
+      <?php foreach ($jurusan["siswa"] as $output){?>
+        <tr>
+      
+          <td><?php echo $output["nama"]?></td>
+          <td><?php echo $output["umur"]?></td>
+        </tr>
+        <?php }; ?>
+      </tbody>
+    </table>
+    <?php }; ?>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html> 
